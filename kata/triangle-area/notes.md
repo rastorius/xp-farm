b99157f915d7c4ab684508da554ced461ad05c31
David Meszaros <david.mark.meszaros@ibm.com>
2021-06-30 13:26:50 +0200

    Test case 2 - GREEN

--- kata/triangle-area/notes.md
+++ kata/triangle-area/notes.md
@@ -5,15 +5,18 @@
 
 # Test cases
 * :D - 1 -> 0.5
-* :| - 2 -> 2.0
+* :D - 2 -> 2.0
 * :( - 3 -> 4.5
 * :( - 7 -> 24.5
 
 # Implementation ideas
-:( - ***get_length function***  
+:D - ***get_length function***  
 Input: triangle string  
 Output: length of triangle's side
 
 :| - ***t_area function***  
+Calls calculate_area with get_length
+
+:( - ***calculate_area function***  
 area(1) = 0.5  
 area(n + 1) = area(n) + n - 0.5
--- kata/triangle-area/src/main/java/com/kata/trianglearea/TriangleAreaCalculator.java
+++ kata/triangle-area/src/main/java/com/kata/trianglearea/TriangleAreaCalculator.java
@@ -4,7 +4,16 @@ public class TriangleAreaCalculator {
     private TriangleAreaCalculator() {
     }
 
+    private static int get_length(String triangle) {
+        return triangle.split("\n").length - 1;
+    }
+
     public static float t_area(String triangle) {
-        return 0.5F;
+        int length = get_length(triangle);
+        if (length == 1) {
+            return 0.5F;
+        } else {
+            return 2.0F;
+        }
     }
 }
