c5100c22669e53017e9bf9cc3663385ca74a4a8c
David Meszaros <david.mark.meszaros@ibm.com>
2021-07-13 16:58:26 +0200

    Test case 5 - REFACTOR

--- kata/number-of-rectangles-in-a-grid/src/main/java/com/kata/rectangles/Rectangles.java
+++ kata/number-of-rectangles-in-a-grid/src/main/java/com/kata/rectangles/Rectangles.java
@@ -6,9 +6,9 @@ public class Rectangles {
 
     public static int numberOfRectangles(int m, int n) {
         var counter = 0;
-        for (var i = 1; i <= m; ++i) {
-            for (var j = 1; j <= n; ++j) {
-                counter += (m - i + 1) * (n - j + 1);
+        for (var i = 0; i < m; ++i) {
+            for (var j = 0; j < n; ++j) {
+                counter += (m - i) * (n - j);
             }
         }
         return counter;
