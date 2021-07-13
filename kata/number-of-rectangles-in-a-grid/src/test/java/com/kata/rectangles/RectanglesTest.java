bd158a98cc5b35913caae46e2a9e4622faf4eecf
David Meszaros <david.mark.meszaros@ibm.com>
2021-07-13 14:42:02 +0200

    Test case 1 - RED

--- kata/number-of-rectangles-in-a-grid/src/test/java/com/kata/rectangles/RectanglesTest.java
+++ kata/number-of-rectangles-in-a-grid/src/test/java/com/kata/rectangles/RectanglesTest.java
@@ -1,13 +1,25 @@
 package com.kata.rectangles;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("Unit tests for rectangles in a grid calculator")
 class RectanglesTest {
 
     @Test
-    void test() {
-        assertThat(1).isEqualTo(1);
+    @DisplayName("When calculate rectangles for a 1x1 grid then should return 1")
+    void given1x1Grid_whenNumberOfRectangles_thenShouldReturn1() {
+        // given
+        int m = 1;
+        int n = 1;
+        int expectedResult = 1;
+
+        // when
+        int actualResult = Rectangles.numberOfRectangles(m, n);
+
+        // then
+        assertThat(actualResult).isEqualTo(expectedResult);
     }
 }
