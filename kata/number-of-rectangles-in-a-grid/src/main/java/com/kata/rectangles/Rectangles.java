ef0c070fb976e121f9198e8f823b6872e2d1fd39
David Meszaros <david.mark.meszaros@ibm.com>
2021-07-13 15:13:11 +0200

    Test case 4 - GREEN

--- kata/number-of-rectangles-in-a-grid/src/main/java/com/kata/rectangles/Rectangles.java
+++ kata/number-of-rectangles-in-a-grid/src/main/java/com/kata/rectangles/Rectangles.java
@@ -5,10 +5,13 @@ public class Rectangles {
     }
 
     public static int numberOfRectangles(int m, int n) {
-        if (n * m == 2) {
-            return 3;
-        } else {
-            return 1;
+        int counter = 0;
+        for(int i = 1; i <= m; ++i) {
+            for(int j = 1; j <= n; ++j) {
+                counter += (m - i + 1) * (n - j + 1);
+            }
         }
+
+        return counter;
     }
 }
