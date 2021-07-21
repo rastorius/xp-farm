dc70d2de837ecd20233ab7414a4992af96a68eee
David Meszaros <david.mark.meszaros@ibm.com>
2021-07-21 14:46:45 +0200

    Test case 3 - REFACTOR

--- kata/mumbling/src/test/java/com/kata/mumbling/MumblingTest.java
+++ kata/mumbling/src/test/java/com/kata/mumbling/MumblingTest.java
@@ -2,11 +2,30 @@ package com.kata.mumbling;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Mumbling unit tests")
 class MumblingTest {
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("A", "A"),
+                Arguments.of("d", "D")
+        );
+    }
+
+    @ParameterizedTest(name = "{0} -> {1}")
+    @MethodSource("testData")
+    @DisplayName("When accum with non-empty input then should return correct string")
+    void givenInput_whenMumbling_thenShouldReturnCorrectString(String input, String expectedResult) {
+        assertThat(Mumbling.accum(input)).isEqualTo(expectedResult);
+    }
 
     @Test
     @DisplayName("When accum with empty input then should return empty string")
@@ -14,34 +33,6 @@ class MumblingTest {
         // given
         String input = "";
         String expectedResult = "";
-
-        // when
-        String actualResult = Mumbling.accum(input);
-
-        // then
-        assertThat(actualResult).isEqualTo(expectedResult);
-    }
-
-    @Test
-    @DisplayName("When accum with a single capital character then should return same character")
-    void given1CapitalCharInput_whenMumbling_thenShouldReturnSameChar() {
-        // given
-        String input = "A";
-        String expectedResult = "A";
-
-        // when
-        String actualResult = Mumbling.accum(input);
-
-        // then
-        assertThat(actualResult).isEqualTo(expectedResult);
-    }
-
-    @Test
-    @DisplayName("When accum with a single lower case character then should return same character converted to upper case")
-    void given1LowerCaseCharInput_whenMumbling_thenShouldReturnSameCharUpperCase() {
-        // given
-        String input = "d";
-        String expectedResult = "D";
 
         // when
         String actualResult = Mumbling.accum(input);
