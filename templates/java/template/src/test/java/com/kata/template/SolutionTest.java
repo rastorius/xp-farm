1fc568efcf0d2ca289e529e4a36008d3663d5216
David Meszaros <david.mark.meszaros@ibm.com>
2021-10-14 08:40:49 +0200

    Add dummy template test to UT class

--- templates/java/template/src/test/java/com/kata/template/SolutionTest.java
+++ templates/java/template/src/test/java/com/kata/template/SolutionTest.java
@@ -1,4 +1,19 @@
 package com.kata.template;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SolutionTest {
+    @Test
+    void given_when_then() {
+        // given
+        int expectedResult = 2;
+
+        // when
+        int actualResult = 1 + 1;
+
+        // then
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
 }
