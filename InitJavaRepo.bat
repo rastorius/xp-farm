3c73769d2632f80c04c2159f3943223d81c19a32
David Meszaros <david.mark.meszaros@ibm.com>
2021-11-09 16:48:56 +0100

    Add remote repo creation to Java init script

--- InitJavaRepo.bat
+++ InitJavaRepo.bat
@@ -1,6 +1,11 @@
 IF "%1"=="" GOTO args_count_wrong
+IF "%2"=="" GOTO args_count_wrong
 
+SET GITHUB_ACCESS_TOKEN=%2
 SET TARGET_FOLDER=..\yellow-belt-katas\%1
+SET REMOTE_REPO=david-mark-meszaros-%1
+SET GITHUB_API_URL=https://api.github.ibm.com
+SET CURL_CMD=curl -H "Authorization: token %GITHUB_ACCESS_TOKEN%" -H "Accept: application/vnd.github.v3+json"
 ECHO TARGET_FOLDER=%TARGET_FOLDER%
 
 IF NOT EXIST "%TARGET_FOLDER%" (mkdir "%TARGET_FOLDER%") ELSE (GOTO folder_already_exists)
@@ -14,12 +19,18 @@ CD "%TARGET_FOLDER%"
 git add .
 git commit -m "Init project"
 
+%CURL_CMD% -X POST %GITHUB_API_URL%/orgs/xpfarm/repos -d "{\"name\":\"%REMOTE_REPO%\"}"
+%CURL_CMD% -X PUT %GITHUB_API_URL%/teams/64917/repos/xpfarm/%REMOTE_REPO%
+ 
+git remote add origin git@github.ibm.com:xpfarm/%REMOTE_REPO%.git
+git push -u origin master
+
 START /b idea64 .
 
 EXIT /b 0
 
 :args_count_wrong
-ECHO ERROR - I need exactly one command line arguments: repository name!
+ECHO ERROR - I need exactly 2 command line arguments: repository name and GitHub access token!
 EXIT /b 1
 
 :folder_already_exists
--- templates/java/template/README.md
+++ templates/java/template/README.md
@@ -1,12 +1,12 @@
 # Kata and requirements
 
 # User stories
-### #1 Validating the input
+### 📘 #1 Validating the input
 As a  
 I want to validate the input  
 So that I
 
-#### #1 Scenario
+#### 📜 #1 Scenario
 **GIVEN**  
 **WHEN**  
 **THEN** 
