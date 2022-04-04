1e69dc65698509b6d8798022e52428a97baf51c0
David Meszaros <david.mark.meszaros@ibm.com>
2022-04-04 15:19:28 +0200

    feat(template): commit message hook, node js init script

--- InitNodeJsRepo.bat
+++ InitNodeJsRepo.bat
@@ -2,15 +2,15 @@ IF "%1"=="" GOTO args_count_wrong
 IF "%2"=="" GOTO args_count_wrong
 
 SET GITHUB_ACCESS_TOKEN=%2
-SET TARGET_FOLDER=..\yellow-belt-katas\%1
-SET REMOTE_REPO=david-mark-meszaros-%1
-SET GITHUB_API_URL=https://api.github.ibm.com
+SET TARGET_FOLDER=..\orange-belt-katas\%1
+SET REMOTE_REPO=orange-belt-katas-%1
+SET GITHUB_API_URL=https://api.github.com
 SET CURL_CMD=curl -H "Authorization: token %GITHUB_ACCESS_TOKEN%" -H "Accept: application/vnd.github.v3+json"
 ECHO TARGET_FOLDER=%TARGET_FOLDER%
 
 IF NOT EXIST "%TARGET_FOLDER%" (mkdir "%TARGET_FOLDER%") ELSE (GOTO folder_already_exists)
 
-XCOPY /s templates\java\template "%TARGET_FOLDER%"
+XCOPY /s templates\node-js "%TARGET_FOLDER%"
 
 git init "%TARGET_FOLDER%"
 
@@ -21,13 +21,14 @@ CD "%TARGET_FOLDER%"
 git add .
 git commit -m "chore: init project"
 
-%CURL_CMD% -X POST %GITHUB_API_URL%/orgs/xpfarm/repos -d "{\"name\":\"%REMOTE_REPO%\"}"
-%CURL_CMD% -X PUT %GITHUB_API_URL%/teams/64917/repos/xpfarm/%REMOTE_REPO%
+%CURL_CMD% -X POST %GITHUB_API_URL%/user/repos -d "{\"name\":\"%REMOTE_REPO%\"}"
  
-git remote add origin git@github.ibm.com:xpfarm/%REMOTE_REPO%.git
+git remote add origin https://github.com/rastorius/%REMOTE_REPO%.git
 git push -u origin master
 
-START /b idea64 .
+START /b webstorm64 .
+
+npm install
 
 EXIT /b 0
 
